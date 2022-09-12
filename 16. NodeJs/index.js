a968630de78d8c6e3f0da9ace60dce4f2fcf016b
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-12 22:25:40 +0530

    Intro to node

---
+++ 16. NodeJs/index.js
@@ -0,0 +1,14 @@
+/* 
+    - type node init in terminal to begin with a node package 
+    - to incorpoprate an exteranl package use npm
+    - npm install package name
+    - example: npm install superheroes
+    - this will install the package and add the dependency to the package.json file
+    - use require function to include a package
+*/
+
+var superhero = require('superheroes');
+// the above line of code includes the package superheroes 
+for(let i=0; i<20;i++)
+console.log(superhero.random());
+// we are using an inbuilt method of the package which basically returns a superhero name
