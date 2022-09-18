7bff8e0c94374cbe72cfdb57c1148d479857dc53
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-18 15:16:34 +0530

    Getting started with EJS

---
+++ 19. EJS/app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express();
+// install ejs module and add below line in your code
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// create a view directory, view engine will first see the template files in that directory to load up
+
+
+
+app.get("/", function (req, res) {
+    // similar to django template rendering
+    // var today = new Date().getDay();
+    var today = Math.floor(Math.random()*7);
+    // try changing the value of today in the range between 0-6 you will see output difference
+    var day = "";
+    switch (today) {
+        case 0: day = "Sunday"; break;
+        case 1: day = "Monday"; break;
+        case 2: day = "Tuesday"; break;
+        case 3: day = "Wednesday"; break;
+        case 4: day = "Thrusday"; break;
+        case 5: day = "Friday"; break;
+        case 6: day = "Saturday"; break;
+        default: console.log("Error!");
+    }
+    res.render('list', { day: day });
+});
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000!");
+});
