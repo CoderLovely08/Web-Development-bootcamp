8db7f2a9308d24f5524e0c5bf123c7e2242e9cf4
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-18 18:33:51 +0530

    code optimization

--- 19. EJS/date.js
+++ 19. EJS/date.js
@@ -1,21 +1,17 @@
-module.exports.getDate = getDate;
-function getDate() {
-    let today = new Date();
-    let options = {
+module.exports.getDate = function() {
+    const today = new Date();
+    const options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-    let day = today.toLocaleString("en-US", options);
-    return day;
+    return today.toLocaleString("en-US", options);
 }
 
-module.exports.getDay = getDay;
-function getDay() {
-    let today = new Date();
-    let options = {
+module.exports.getDay = function () {
+    const today = new Date();
+    const options = {
         weekday: "long",
     };
-    let day = today.toLocaleString("en-US", options);
-    return day;
+    return today.toLocaleString("en-US", options);
 }
