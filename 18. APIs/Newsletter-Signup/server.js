41b42888d5a041ba78a6b914f3e26c417cf7477c
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-18 14:23:06 +0530

    completed mailchimp api teting

--- 18. APIs/Newsletter-Signup/server.js
+++ 18. APIs/Newsletter-Signup/server.js
@@ -14,9 +14,8 @@ app.get("/",function(req,res){
 
 
 app.use("/",function(req,res){
-    const fullName = req.body.nameInput;
-    const fname= fullName.split(" ")[0];
-    const lname= fullName.split(" ")[1];
+    const fname= req.body.fnameInput;
+    const lname= req.body.lnameInput;
     const userEmail = req.body.emailInput;
     
     const data = {
@@ -39,6 +38,11 @@ app.use("/",function(req,res){
         auth: "lovely:f4fa3978bb4f5e8b0973a5e02e0954cb-us10"
     }
     const request = https.request(url,options,function(response){
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/success.html")
+        }else{
+            res.sendFile(__dirname+"/failure.html")
+        }
         response.on("data",function(data){
             console.log(JSON.parse(data));
         })
@@ -49,6 +53,9 @@ app.use("/",function(req,res){
 });
 
 
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
 // API Key
 // f4fa3978bb4f5e8b0973a5e02e0954cb-us10
 
