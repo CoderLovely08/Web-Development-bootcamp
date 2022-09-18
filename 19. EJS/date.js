51f1bbf7e6577b81526ca78c89f28af7e762b16d
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-18 18:21:51 +0530

    learned to create a module and use it

---
+++ 19. EJS/date.js
@@ -0,0 +1,21 @@
+module.exports.getDate = getDate;
+function getDate() {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleString("en-US", options);
+    return day;
+}
+
+module.exports.getDay = getDay;
+function getDay() {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+    };
+    let day = today.toLocaleString("en-US", options);
+    return day;
+}
