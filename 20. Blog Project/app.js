8b418fc0b871a1e401bcc8d958e578125686e9e1
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-18 19:03:48 +0530

    Added header and footer

--- 20. Blog Project/app.js
+++ 20. Blog Project/app.js
@@ -12,11 +12,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-    res.render("home");
+app.get("/", function (req, res) {
+    res.render("home", {content: homeStartingContent});
 })
 
 
@@ -30,6 +30,6 @@ app.get("/",function(req,res){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
