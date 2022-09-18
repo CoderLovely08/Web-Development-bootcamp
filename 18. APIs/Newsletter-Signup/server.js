ca328a0237b0ccff4f9fafffe826ff3611ac5be3
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-18 13:31:35 +0530

    Mailchimp add audience succesfull

--- 18. APIs/Newsletter-Signup/server.js
+++ 18. APIs/Newsletter-Signup/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-// const request = require("request")
-// const { urlencoded } = require("body-parser")
+const https = require("https")
 
 const app = express()
 
@@ -11,21 +10,42 @@ app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
-    const mailchimpTx = require("mailchimp_transactional")("f4fa3978bb4f5e8b0973a5e02e0954cb-us10");
-
-async function run() {
-  const response = await mailchimpTx.users.ping();
-  console.log(response);
-}
-
-run();
 });
 
 
 app.use("/",function(req,res){
-    var fullName = req.body.nameInput;
-    var userEmail = req.body.emailInput;
-    console.log(fullName);
+    const fullName = req.body.nameInput;
+    const fname= fullName.split(" ")[0];
+    const lname= fullName.split(" ")[1];
+    const userEmail = req.body.emailInput;
+    
+    const data = {
+        members: [
+            {
+                email_address: userEmail,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+    
+    const url = "https://us10.api.mailchimp.com/3.0/lists/9178bac9ae";
+    const options = {
+        method: "POST",
+        auth: "lovely:f4fa3978bb4f5e8b0973a5e02e0954cb-us10"
+    }
+    const request = https.request(url,options,function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
 });
 
 
@@ -33,11 +53,7 @@ app.use("/",function(req,res){
 // f4fa3978bb4f5e8b0973a5e02e0954cb-us10
 
 // List ID
-// 
-
-
-
-
+// 9178bac9ae
 
 
 
