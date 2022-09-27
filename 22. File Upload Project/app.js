439265b1e630c073d6ace2b39087ecef28f8fcdf
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-27 20:10:18 +0530

    Move to temp folder done

---
+++ 22. File Upload Project/app.js
@@ -0,0 +1,45 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const fileupload = require("express-fileupload")
+// const loadsh = require("lodash")
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(fileupload());
+
+/*-------------------------------------------
+                Global Variables
+  -------------------------------------------*/
+
+
+/*-------------------------------------------
+                Home route
+  -------------------------------------------*/
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.post("/upload", function (req, res) {
+    console.log(req.files);
+    if (!req.files) {
+        return res.status(400).send("No files Found!");
+    }
+    let myfile = req.files.mysamplefile;
+    let uploadPath = __dirname + "/uploads/" + myfile.name;
+    myfile.mv(uploadPath, function (err) {
+        if (err) console.log("Error!");
+        console.log("Saved to folder");
+    })
+})
+
+
+app.listen(3001, function () {
+    console.log("Server is running on port 3001!");
+})
