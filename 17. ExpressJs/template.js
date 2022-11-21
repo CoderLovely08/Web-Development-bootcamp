f0fa79b72f5f78e6461de91084d2edb9d58411e4
Lovely Sharma <lovelysingeras297@gmail.com>
2022-11-21 19:26:38 +0530

    Update template.js

--- 17. ExpressJs/template.js
+++ 17. ExpressJs/template.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const bodyParser = requrie("body-parser")
+const bodyParser = require("body-parser")
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
