0c4cbd6ac0889aaa995200bca18e44d85011383a
Lovely Sharma <lovelysingeras297@gmail.com>
2023-05-30 18:53:08 +0530

    Added basic react starter guide

--- 18. APIs/WeatherApp/server.js
+++ 18. APIs/WeatherApp/server.js
@@ -3,34 +3,35 @@ const https = require("https");
 const bodyParser = require("body-parser")
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname + "/index.html")
 });
 
-app.post("/",function(req,res){
+app.post("/", function (req, res) {
     const query = req.body.cityInput;
     const apiKey = "7c481d9ac1b552236f593886500f6b72";
     const unit = "metric"
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
-    https.get(url,function(response){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function (response) {
         console.log(response.statusCode);
-        
-        response.on("data",function(data){
+
+        response.on("data", function (data) {
+            console.log(data);
             const weatherData = JSON.parse(data);
             console.log(weatherData);
-            const temperature=weatherData.main.temp;
+            const temperature = weatherData.main;
             console.log(temperature);
             // we can only use res.send once but we can use multiple res.write
-            res.send("The current temperature in "+ query +" is "+temperature+" degrees celcius.")
+            res.send("The current temperature in " + query + " is " + temperature + " degrees celcius.")
         });
     });
 })
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000.");
 });
---
+++ 23. React/README.md
@@ -0,0 +1,70 @@
+# React Notes
+
+Initially start practising on [CodeSandBox.io](https://codesandbox.io/) by creating a new react app.
+
+## 1. Getting started
+### 1. Basic App
+```javascript
+// Import the following to get started
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Target the root element
+const roolElement = document.getElementById("root");
+
+// Prepare content
+// Only one element can go in the top body, so nest everything inside a div
+const content = (
+  <div>
+    <h1>Hi this is a heading</h1>
+    <p>This is a paragraph.</p>
+  </div>
+);
+
+// To render content use the ReactDOM with render() method which takes 2 params
+// @param 1 - What to display (your content)
+// @param 2 - Where to display (your target element)
+ReactDOM.render(content, roolElement);
+```
+
+- Output
+
+    ![Basic App](images/basic1.png)
+
+
+
+### 2. Using Variables
+```javascript
+// Import the following to get started
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Target the root element
+const roolElement = document.getElementById("root");
+
+// To inject a value of a variable, use {varName} 
+const foodItem = "Sandwich";
+
+// Prepare content
+// Only one element can go in the top body so, nest everything inside a div
+const content = (
+  <div>
+    <b>Foods you might like</b>
+    <ul>
+      <li>Idli</li>
+      <li>Wada</li>
+      <li>{foodItem}</li>
+    </ul>
+  </div>
+);
+
+// To render content use the ReactDOM with render() method which takes 2 params
+// @param 1 - What to display (your content)
+// @param 2 - Where to display (your target element)
+ReactDOM.render(content, roolElement);
+
+```
+
+- Output
+
+    ![Basic App](images/basic2.png)
